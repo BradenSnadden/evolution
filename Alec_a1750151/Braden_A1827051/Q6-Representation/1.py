bc9eb9d6120681e53f75815c243750b77625ca87
Alec <a1750151@student.adelaide.edu.au>
2023-08-18 16:06:21 +0930

    6 update

---
+++ Alec_a1750151/Braden_A1827051/Q6-Representation/1.py
@@ -0,0 +1,15 @@
+import os
+#import numpy as np
+
+f = open("tsp.txt", "r")
+
+#cities = numpy.genfromtxt(fname = f, skip_header=7)
+#print(cities)
+
+#class TSP:
+    #cities = []
+    #for line in f.readlines():
+       # cities.append(line.split(' '))
+
+       # print(cities)
+
