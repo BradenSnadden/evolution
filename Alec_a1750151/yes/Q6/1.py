7d35d7f0d8335261bc05fa7523b81d400d53dc2d
Alec <a1750151@student.adelaide.edu.au>
2023-08-18 16:31:05 +0930

    fix

--- Alec_a1750151/yes/Q6/1.py
+++ Alec_a1750151/yes/Q6/1.py
@@ -1,7 +1,16 @@
 import os
+from pathlib import Path
 #import numpy as np
 
-f = open("tsp.txt", "r")
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+os.getcwd()
+
+#f = open('tsp.txt', 'r')
+
+
 
 #cities = numpy.genfromtxt(fname = f, skip_header=7)
 #print(cities)
