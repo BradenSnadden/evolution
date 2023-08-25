fb711a01afcc7a9e6f7cfec80acb10d66ec12e46
Alec <a1750151@student.adelaide.edu.au>
2023-08-25 15:49:13 +0930

    add comment

---
+++ Final Mutation/6.py
@@ -0,0 +1,13 @@
+class TSP:
+    import os
+    from pathlib import Path
+    import numpy as np
+
+    # change directory to file location
+    os.chdir(os.path.dirname(__file__))
+
+    # create cities array from data in tsp file
+    cities = np.genfromtxt("tsp.txt", skip_header=6, skip_footer=1)
+    print(cities)
+
+TSP()
