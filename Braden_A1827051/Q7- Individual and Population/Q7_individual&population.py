b01a8410da5e439c206f67adc187882858eab39c
Kaijun0515cc <141392915+Kaijun0515cc@users.noreply.github.com>
2023-08-25 15:43:57 +0930

    Add files via upload

---
+++ Braden_A1827051/Q7- Individual and Population/Q7_individual&population.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+class Individual:
+
+    def __init__(self, n):
+        self.length = n
+        self.generate()
+        
+    def generate(self):
+        self.seq = np.random.permutation(self.length)
+
+class Population:
+    def __init__(self, n):
+        self.size = n
+    
+    def init_pop(self):
+        self.pop_list = [Individual(self.size) for _ in range(self.size)]
+
+if __name__ == "__main__":
+    i = Individual(10)
+    i.generate()
+    print(i.seq)
