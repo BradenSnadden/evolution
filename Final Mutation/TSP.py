0d75562bd1d08d5fb85425ca060f17a49593f952
Alec <a1750151@student.adelaide.edu.au>
2023-08-25 16:04:38 +0930

    added number of rows

--- Final Mutation/TSP.py
+++ Final Mutation/TSP.py
@@ -9,6 +9,10 @@ class TSP:
     # create cities array from data in tsp file
     cities = np.genfromtxt("tsp.txt", dtype=float, skip_header=6, skip_footer=1)
     cities[:,1:]
+
+    #N = number of rows
+    N, num_cols = cities.shape
+
     print(cities)
 
 TSP()
